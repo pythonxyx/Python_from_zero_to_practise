1a5e0c301e189f7b86f860d1c784c26fd578abfe
pythonxyx <xyxgary@126.com>
2020-04-17 00:35:17 +0800

    对requests模块的测试加入

--- requests模块测试.py
+++ requests模块测试.py
@@ -8,20 +8,20 @@ html = response.content.decode('GB2312')
 html_soup = BeautifulSoup(html,'html.parser')
 html_soup_find = html_soup.find_all('div',class_="co_content8")[0]
 # print(html_soup_find[0])
-ruler = re.compile(r'<a href="(/html/gndy/dyzz/\d+.*.html)">(.*?)</a><br/>')
+ruler = re.compile(r'<a href="(/html/gndy/dyzz/(\d+).*.html)">(.*?)</a><br/>')
 item = str(html_soup_find)
 find_txt = re.findall(ruler,item)
-# print(list(find_txt[0]))
+print(list(find_txt))
 
-dizhilist = []
-namelist = []
-
-for i in range(len(find_txt)):
-    new = list(find_txt[i])
-    dizhilist.append(new[0])
-    namelist.append(new[1])
-print(dizhilist)
-print(namelist)
+# dizhilist = []
+# namelist = []
+#
+# for i in range(len(find_txt)):
+#     new = list(find_txt[i])
+#     dizhilist.append(new[0])
+#     namelist.append(new[1])
+# print(dizhilist)
+# print(namelist)
 
 
 
